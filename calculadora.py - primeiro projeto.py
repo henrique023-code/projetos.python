25c1c5d9e97642e453160ec197c8e570b9e6027d
henrique023-code <henriquedesousa18@gmail.com>
2026-06-18 14:21:56 -0300

    Add files via upload

---
+++ calculadora.py - primeiro projeto.py
@@ -0,0 +1,14 @@
+num1 = float(input("Digite o primeiro numero: "))
+operacao = input('Digite a operação(+, -, *, /): ')
+num2 = float(input("Digite o segundo número: "))
+
+if operacao == '+':
+  print(num1 + num2)
+elif operacao == '-':
+  print(num1 - num2)
+elif operacao == '*':
+  print(num1 * num2)
+if num2 != 0:
+    print(num1 / num2)
+else:
+    print('Não é possivel dividir por zero')
